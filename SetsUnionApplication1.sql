18500c3f3c804fed1d8deb0e69881925d30cceb9
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-05-27 21:00:13 +0530

    UNION application in databases for sales data

---
+++ SetsUnionApplication1.sql
@@ -0,0 +1,31 @@
+select 
+       [OrderID],
+       GotFrom='Orders'
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+from Sales.Orders
+UNION
+select [OrderID],
+       GotFrom='OrdersArchives'
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+from Sales.OrdersArchive
+order by OrderID;
