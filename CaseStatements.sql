6ee8d31137643466ee8684b2f726bd3aeda72894
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-06-01 12:21:57 +0530

    Case Statement Task

--- CaseStatements.sql
+++ CaseStatements.sql
@@ -2,12 +2,16 @@
 showing total sales for each of the following categories
 High(over 50) medium(21-50) low(20 or less)
 sort the categories from highest sales to the lowest*/
-Select 
-	OrderID, Sales,
-	Case
-			When Sales>50 THEN 'High'
-			When Sales>20THEN 'Medium'
-			ELSE 'Low'
-	End [Sales Category]
-From Sales.Orders 
-Order by Sales DESC
+Select [Sales Category],
+SUM(Sales) as TotalSales
+FROM
+	(Select 
+		OrderID, Sales,
+		Case
+				When Sales>50 THEN 'High'
+				When Sales>20THEN 'Medium'
+				ELSE 'Low'
+		End [Sales Category]
+	From Sales.Orders)t
+GROUP BY [Sales Category]
+ORDER BY TotalSales DESC
