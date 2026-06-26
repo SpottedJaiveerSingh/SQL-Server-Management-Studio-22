55d7a7c2afa9e8facebc24e6f32dd7b4734dfa1d
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-06-26 17:43:04 +0530

    Create StoredProcedures.sql

---
+++ StoredProcedures.sql
@@ -0,0 +1,41 @@
+--Step 1: Write a Query
+--For US Customers find the total num of customers and the average score
+Select 
+Count(*) TotalCustomers,
+AVG(Score) AvgScore
+From Sales.Customers
+Where Sales.Customers.Country='USA';
+
+--Step 2: Turning the Query into a stored procedure
+CREATE PROCEDURE GetCustomerSummary as 
+begin
+Select 
+Count(*) TotalCustomers,
+AVG(Score) AvgScore
+From Sales.Customers
+Where Sales.Customers.Country='USA'
+end;
+--Step 3: Execute  the Strored Procedure
+EXEC GetCustomerSummary;
+--For German Customers find the total num of customers and average score
+
+--Define Stored procedure
+ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA' As 
+BEGIN
+Select 
+	Count(*) TotalCustomers,
+	AVG(Score) AvgScore
+From Sales.Customers
+Where Sales.Customers.Country=@Country ;
+--Find the total nunber of orders and total sales
+SELECT 
+Count(OrderID) TotalOrders,
+Sum(Sales) TotalSales
+From Sales.Orders o
+JOIN Sales.Customers c
+On c.CustomerID=o.CustomerID
+where c.Country=@Country
+END
+
+EXEC GetCustomerSummary; 
+EXEC GetCustomerSummary @Country = 'Germany';
