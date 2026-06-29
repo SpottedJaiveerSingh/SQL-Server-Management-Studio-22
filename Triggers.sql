aaada0ca565d0137f80b81887f0e39975f1a546c
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-06-29 21:59:49 +0530

    Create Triggers.sql

---
+++ Triggers.sql
@@ -0,0 +1,8 @@
+--STEP 1 Create Log Table
+--Understanding the Triggers
+Create table Sales.EmployeeLogs(
+	LogID int identity(1,1)Primary key,
+	EmployeeID int,
+	LogMessage Varchar(255),
+	LogDate DATE
+)
