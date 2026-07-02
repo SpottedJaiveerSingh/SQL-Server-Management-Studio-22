44fa7638e9c43b12745d2723ebd9e5ed51a52bff
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-07-02 10:29:32 +0530

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -1,9 +1,23 @@
 --STEP 1 Create Log Table
---Understanding the Triggers
 Create table Sales.EmployeeLogs(
 	LogID int identity(1,1)Primary key,
 	EmployeeID int,
 	LogMessage Varchar(255),
 	LogDate DATE
-) 
---lets do it...!!!
+);
+--STEP 2 Create Trigger on employees table
+Create Trigger trg_AfterInsertEmployee on Sales.Employees
+AFTER INSERT AS 
+BEGIN
+	INSERT INTO Sales.EmployeeLogs(EmployeeID, LogMessage, LogDate)
+	SELECT 
+		EmployeeID,
+		'New Employee Added ='+ CAST(EmployeeID as varchar),
+		GETDATE()
+		FROM INSERTED
+END
+--STEP 3 INSERT NEW DATA INTO EMPLOYEES
+Select * From Sales.EmployeeLogs
+Insert into Sales.Employees
+Values
+(6, 'Maria', 'Doe', 'HR', '1988-01-12','F',80000,3 )
