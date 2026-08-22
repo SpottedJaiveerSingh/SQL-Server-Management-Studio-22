326704a93eeb8f8881375105a3ea13088a0c5ac7
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-08-22 09:43:17 +0530

    Update Index1.sql

--- Index1.sql
+++ Index1.sql
@@ -25,3 +25,22 @@ on Sales.DBCustomers
 CREATE NONCLUSTERED COLUMNSTORE INDEX idx_DBCustomers_FirstName
 on Sales.DBCustomers (FirstName)
 -------------------------------------------------------------------------------------------------------------------------------------
+--heap
+use AdventureWorksDW2025
+Select *
+into FactInternetSales_HP
+from FactInternetSales
+
+--Rowstore
+Select *
+into FactInternetSales_RS
+from FactInternetSales
+Create Clustered Index idx_FactInternetSales_RS_PK
+On FactInternetSales_RS (SalesOrderNumber, SalesOrderLineNumber)
+
+--Columnstore
+Select *
+into FactInternetSales_CS
+from FactInternetSales
+Create Clustered Columnstore Index idx_FactInternetSales_CS_PK
+On FactInternetSales_CS
