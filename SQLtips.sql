20d03cec472dc4333fc698d849d08e9619e7fc14
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-09-10 18:54:06 +0530

    Update SQLtips.sql

--- SQLtips.sql
+++ SQLtips.sql
@@ -1,11 +1,12 @@
 --tips for performance in SQL
 --1. Select what is needed
-SELECT * from Sales.Customers
-SELECT CustomerID, FirstName, Country FROM Sales.Customers
+SELECT * from Sales.Customers;
+SELECT CustomerID, FirstName, Country FROM Sales.Customers;
 --2. avoid not so needed DISTINCT and ORDER BY
 --3. for exploration limit the rows
-SELECT top 3 * from Sales.Customers
+SELECT top 3 * from Sales.Customers;
 --4. create nonclustered index on frequently used columns in where clause
 --5. avoid applying functions to column in where clause (func on columns can block index usage)
+--avoid leading wildcars as they prevert index usage
 
 
