3044cf2d79828cc2e035729c4e4e001f1f8219c0
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-09-12 20:26:11 +0530

    Update SQLtips.sql

--- SQLtips.sql
+++ SQLtips.sql
@@ -7,6 +7,15 @@ SELECT CustomerID, FirstName, Country FROM Sales.Customers;
 SELECT top 3 * from Sales.Customers;
 --4. create nonclustered index on frequently used columns in where clause
 --5. avoid applying functions to column in where clause (func on columns can block index usage)
---avoid leading wildcars as they prevert index usage
+--6. avoid leading wildcars as they prevert index usage
+--7. use IN instead of multiple OR s 
+--8. JOIN SPEEDs (use inner when possible)
+--best to worst
+--inner>left,right>>worst
+--9. use explicit JOIN (ANSI join) instead of implicit join (NON ANSI join)-- which have written INNERJOIN etc
+--10. Ensure that the columns used in ON clause are indexed.
+--11. filter before joining (big tables)
+--the thing to
+
 
 
