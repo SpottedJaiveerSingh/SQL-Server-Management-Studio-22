c2b26e27bc96820395f058d0b50bd0e19ff28a4c
Jaiveer Singh <spottedjaiveersingh@gmail.com>
2026-09-13 14:11:36 +0530

    Final SQL tips.

--- SQLtips.sql
+++ SQLtips.sql
@@ -15,7 +15,28 @@ SELECT top 3 * from Sales.Customers;
 --9. use explicit JOIN (ANSI join) instead of implicit join (NON ANSI join)-- which have written INNERJOIN etc
 --10. Ensure that the columns used in ON clause are indexed.
 --11. filter before joining (big tables)
---the thing to
+--12. aggregate before joining
+--13. use unions instead of ORs while joining table
+--14. check for nested loops and use the SQL hints while necessary
+--15. union all >>> union if duplicates accepted
+--16. union all + distinct     if duplicates not accepted.
+--17. use COLUMNSTORE INDEX for aggregations on large tables
+--18. pre aggregate data and stroe it in new table for reporting
+--19. joins = exists >> IN
+--20. avoid redundant logic in your query
+--21. avoid datatypes varchar and text
+--22. avoid (MAX) uneccerasily large length in datatypes
+--23. use the NOT NULL constraint where applicable
+--24. ensure all your tables have clustered primary key
+--25. create non clustered indexes for the foreign keys which are used frequently
+--26. avoid over indexing.
+--27. drop unused indexes
+--28. update statistics weekly
+--29. reorganixe an rebuild indexes weekly
+--30. Partition large tables (facts) to improve performance
+
+
+
 
 
 
